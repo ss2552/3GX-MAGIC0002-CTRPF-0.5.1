df61afc1a692d6c2b00d2a76b2ecb8c1e6731ad0
esu2552 <seiya0329game@gmail.com>
2025-08-09 20:59:48 +0900

    fix

--- Sources/ctrulib/system/syscalls.c
+++ Sources/ctrulib/system/syscalls.c
@@ -3,6 +3,7 @@
 #include <sys/lock.h>
 #include <sys/reent.h>
 #include <string.h>
+
 #include <3DS.h>
 #include "ctrulib/internal.h"
 
@@ -13,31 +14,15 @@ extern const u8 __tdata_lma[];
 extern const u8 __tdata_lma_end[];
 extern u8 __tls_start[];
 
-static ThreadVars * g_mainThreadVars;
-
 static struct _reent* __ctru_get_reent()
 {
 	ThreadVars* tv = getThreadVars();
-
 	if (tv->magic != THREADVARS_MAGIC)
 	{
-        // We're probably hooked from game so get main thread's reent
-        return (g_mainThreadVars->reent);
+		svcBreak(USERBREAK_PANIC);
+		for (;;);
 	}
 	return tv->reent;
-}
-
-void*     __getThreadLocalStorage(void)
-{
-	ThreadVars* tv = getThreadVars();
-
-	if (tv->magic != THREADVARS_MAGIC)
-	{
-        // We're probably hooked from game so get main thread's tls
-        return g_mainThreadVars->tls_tp;
-	}
-
-	return tv->tls_tp;
 }
 
 void __system_initSyscalls(void)
@@ -63,8 +48,6 @@ void __system_initSyscalls(void)
 	tv->reent = _impure_ptr;
 	tv->thread_ptr = NULL;
 	tv->tls_tp = __tls_start-8; // ARM ELF TLS ABI mandates an 8-byte header
-
-    g_mainThreadVars = tv;
 
 	u32 tls_size = __tdata_lma_end - __tdata_lma;
 	if (tls_size)
